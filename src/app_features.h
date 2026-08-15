60c032f8fd62efb17e97abbaa8ce4ee188963041
Avanish <avanishmg05@gmail.com>
2026-08-15 12:34:57 +0530

    fixed empty app_features.h filled it with the right content

---
+++ src/app_features.h
@@ -0,0 +1,15 @@
+#ifndef FEATURES_H
+#define FEATURES_H
+
+void login();
+
+void createProfile();
+void deleteProfile();
+void viewProfile();
+
+void dashboard();
+
+void viewSettings();
+void updateSettings();
+
+#endif
